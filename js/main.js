994cba23ffb8aec1a97eea91a1d49302c011b9fa
Joss <joss.bleach@outlook.com>
2018-01-20 22:20:56 +0000

    final of the day

--- js/main.js
+++ js/main.js
@@ -20,10 +20,21 @@ $(document).ready(function(){
     //   $('.js-tilt').tilt({
     //       scale: 1.2
     //   })
-    window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
-    }
-
+     window.onbeforeunload = function () {
+       window.scrollTo(0, 0);
+     }
+     $(function initMap() {
+      var myLatLng = {lat: 51.53066, lng: -0.093566};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: myLatLng
+      });
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'SilverCloud Creative'
+              });
+          })
     $(function() {
        //Scrolling Effects
        var heroHeading = $(".hero-heading");
